7642e7afc57e9ad4c8610ee8679934cbb4e8eb15
Nabil <nabilfatur44@gmail.com>
2025-04-09 17:48:12 +0700

    feat: add google as auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+import SignIn from "@/components/shared/sign-in";
+import { auth, signOut } from "@/lib/auth";
 
-export default function Home() {
-  return <div>Home Page</div>;
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <div>
+      <SignIn />
+      {session && (
+        <div>
+          <p>Signed in as {session.user?.email}</p>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <button type="submit">Sign out</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
